a60691c37fad899bf9b124e77fa2385e64fb96db
deniskovvall-hub <deniskovvall@gmail.com>
2026-06-02 23:04:33 +0300

    clean reset and push

---
+++ Lesson4/Lesson/js/script.js
@@ -0,0 +1,27 @@
+// 1 Task
+// function checkAge(age) {
+//   return age > 18 ? true : confirm('Батьки дозволили?');
+// }
+
+// console.log(checkAge(12));
+
+// 2 Task
+
+// function min(a,b){
+//     return a > b ? b : a
+// }
+
+// console.log(min(10,20));
+
+
+// 3 Task
+
+// const ask = (question, yes, no) => confirm(question) ? yes() : no();
+
+// ask(
+//   "Ви згодні?",
+//   () => alert("Ви погодились."),
+//   () => alert("Ви скасували виконання.")
+// );
+
+// console.log(ask);
